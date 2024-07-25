df373c1d423a1834310d0daf272fb15ed548059e
bunnycou <noahjcou@gmail.com>
2024-07-25 01:10:55 -0400

    added caster name support

--- lol/data.js
+++ lol/data.js
@@ -1,11 +1,14 @@
-function loadVars() {
-    var blueName = localStorage.getItem("blue")
-    var blueScore =  localStorage.getItem("blueScore")
-    var redName = localStorage.getItem("red")
-    var redScore = localStorage.getItem("redScore")
-    var patch = localStorage.getItem("patch")
-    var casters = localStorage.getItem("casters")
-    var potw = localStorage.getItem("potw")
+function loadvars() {
+    let blueName = localStorage.getItem("blue")
+    let blueScore =  localStorage.getItem("blueScore")
+    let redName = localStorage.getItem("red")
+    let redScore = localStorage.getItem("redScore")
+    let patch = localStorage.getItem("patch")
+    let casters = localStorage.getItem("casters")
+    let caster1 = localStorage.getItem("caster1")
+    let caster2 = localStorage.getItem("caster2")
+    let caster3 = localStorage.getItem("caster3")
+    let potw = localStorage.getItem("potw")
 
     if (!blueName) { blueName = "Blue" }
     if (!blueScore) { blueScore = 0 }
@@ -13,6 +16,9 @@ function loadVars() {
     if (!redScore) { redScore = 0 }
     if (!patch) { patch = getLatest() }
     if (!casters) { casters = 1 }
+    if (!caster1) { caster1 = "Caster 1" }
+    if (!caster2) { caster2 = "Caster 2" }
+    if (!caster3) { caster3 = "Caster 3" }
     if (!potw) { potw = "Everyone" }
 
     document.getElementById("blue").value = blueName
@@ -21,6 +27,9 @@ function loadVars() {
     document.getElementById("redScore").value = redScore
     document.getElementById("patch").value = patch
     document.getElementById("casters").value = casters
+    document.getElementById("caster1").value = caster1
+    document.getElementById("caster2").value = caster2
+    document.getElementById("caster3").value = caster3
     document.getElementById("potw").value = potw
 }
 
@@ -29,64 +38,55 @@ function getLatest() {
 }
 
 function Get(url){
-    var Httpreq = new XMLHttpRequest(); // a new request
+    let Httpreq = new XMLHttpRequest(); // a new request
     Httpreq.open("GET",url,false);
     Httpreq.send(null);
     return Httpreq.responseText;
 }
 
-function storeVars() {
-    let blueVar = document.getElementById("blue").value
-    let blueScoreVar = document.getElementById("blueScore").value
-    let redVar = document.getElementById("red").value
-    let redScoreVar = document.getElementById("redScore").value
-    let patchVar = document.getElementById("patch").value
-    let castersVar = document.getElementById("casters").value
-    let potwVar = document.getElementById("potw").value
+function storevars() {
+    let blue = document.getElementById("blue").value
+    let blueScore = document.getElementById("blueScore").value
+    let red = document.getElementById("red").value
+    let redScore= document.getElementById("redScore").value
+    let patch = document.getElementById("patch").value
+    let casters= document.getElementById("casters").value
+    let caster1 = document.getElementById("caster1").value
+    let caster2 = document.getElementById("caster2").value
+    let caster3 = document.getElementById("caster3").value
+    let potw = document.getElementById("potw").value
 
-    localStorage.setItem("blue", blueVar)
-    localStorage.setItem("blueScore", blueScoreVar)
-    localStorage.setItem("red", redVar)
-    localStorage.setItem("redScore", redScoreVar)
-    localStorage.setItem("game", parseInt(blueScoreVar)+parseInt(redScoreVar)+1)
-    localStorage.setItem("patch", patchVar)
-    localStorage.setItem("casters", castersVar)
-    localStorage.setItem("potw", potwVar)
-    
-    loadVars();
+    localStorage.setItem("blue", blue)
+    localStorage.setItem("blueScore", blueScore)
+    localStorage.setItem("red", red)
+    localStorage.setItem("redScore", redScore)
+    localStorage.setItem("game", parseInt(blueScore) + parseInt(redScore) + 1)
+    localStorage.setItem("patch", patch)
+    localStorage.setItem("casters", casters)
+    localStorage.setItem("caster1", caster1)
+    localStorage.setItem("caster2", caster2)
+    localStorage.setItem("caster3", caster3)
+    localStorage.setItem("potw", potw)
 }
 
-function swapVars() {
-    let blueVar = document.getElementById("blue").value
-    let blueScoreVar = document.getElementById("blueScore").value
-    let redVar = document.getElementById("red").value
-    let redScoreVar = document.getElementById("redScore").value
-    let patchVar = document.getElementById("patch").value
-    let castersVar = document.getElementById("casters").value
-    let potwVar = document.getElementById("potw").value
+function swapvars() {
+    let blue = document.getElementById("blue").value
+    let blueScore = document.getElementById("blueScore").value
+    let red = document.getElementById("red").value
+    let redScore = document.getElementById("redScore").value
 
-    var tmp
-    tmp = redVar
-    redVar = blueVar
-    blueVar = tmp
-
-    tmp = redScoreVar
-    redScoreVar = blueScoreVar
-    blueScoreVar = tmp
-
-    localStorage.setItem("blue", blueVar)
-    localStorage.setItem("blueScore", blueScoreVar)
-    localStorage.setItem("red", redVar)
-    localStorage.setItem("redScore", redScoreVar)
-    localStorage.setItem("game", parseInt(blueScoreVar)+parseInt(redScoreVar)+1)
-    localStorage.setItem("patch", patchVar)
-    localStorage.setItem("casters", castersVar)
-    localStorage.setItem("potw", potwVar)
+    localStorage.setItem("blue", red)
+    localStorage.setItem("blueScore", redScore)
+    localStorage.setItem("red", blue)
+    localStorage.setItem("redScore", blueScore)
     
-    loadVars();
+    document.getElementById("blue").value = red
+    document.getElementById("blueScore").value = redScore
+    document.getElementById("red").value = blue
+    document.getElementById("redScore").value = blueScore
 }
 
-document.getElementById("submit").addEventListener("click", storeVars);
-document.getElementById("swap").addEventListener("click", swapVars);
+document.getElementById("submit").addEventListener("click", storevars);
+document.getElementById("swap").addEventListener("click", swapvars);
 
-loadVars();
+loadvars();
--- lol/draft.js
+++ lol/draft.js
@@ -1,25 +1,85 @@
-
-
 let blueName = localStorage.getItem("blue")
 let blueScore = localStorage.getItem("blueScore")
 let redName = localStorage.getItem("red")
 let redScore = localStorage.getItem("redScore")
 let casters = localStorage.getItem("casters")
-// let game = localStorage.getItem("game")
-// let patch = localStorage.getItem("patch")
+let caster1 = localStorage.getItem("caster1")
+let caster2 = localStorage.getItem("caster2")
+let caster3 = localStorage.getItem("caster3")
 
 if (!blueName) { blueName = "Blue" }
 if (!blueScore) { blueScore = "0" }
 if (!redName) { redName = "Red" }
 if (!redScore) { redScore = "0" }
-if (!casters) { casters = "1" }
-// if (!game) { game = "Game 1" } else { game = "Game " + game }
-// if (!patch) { patch = "Latest Patch" } else { patch = "Patch " + patch }
+if (!casters) { casters = 1 }
+if (!caster1) { caster1 = "Caster 1" }
+if (!caster2) { caster2 = "Caster 2" }
+if (!caster3) { caster3 = "Caster 3" }
+
+if (casters == 1) {
+    let height = "10vh"
+    let width = "39vw"
+    let bottom = "26vh"
+
+    let caster1Box = document.getElementById("draft_caster1Box")
+    caster1Box.style.height = height
+    caster1Box.style.width = width
+    caster1Box.style.bottom = bottom
+    caster1Box.style.left = "30vw"
+
+    document.getElementById("draft_caster1").textContent = caster1
+
+} else if (casters == 2 ) {
+    let height = "10vh"
+    let width = "28.5vw"
+    let bottom = "34vh"
+
+    let caster1Box = document.getElementById("draft_caster1Box")
+    caster1Box.style.height = height
+    caster1Box.style.width = width
+    caster1Box.style.bottom = bottom
+    caster1Box.style.left = "20.5vw"
+
+    let caster2Box = document.getElementById("draft_caster2Box")
+    caster2Box.style.height = height
+    caster2Box.style.width = width
+    caster2Box.style.bottom = bottom
+    caster2Box.style.left = "51.5vw"
+
+    document.getElementById("draft_caster1").textContent = caster1
+    document.getElementById("draft_caster2").textContent = caster2
+
+} else if (casters == 3) {
+    let height = "10vh"
+    let width = "23vw"
+    let bottom = "22vh"
+    let upper = "54vh"
+
+    let caster1Box = document.getElementById("draft_caster1Box")
+    caster1Box.style.height = height
+    caster1Box.style.width = width
+    caster1Box.style.bottom = upper
+    caster1Box.style.left = "26vw"
+
+    let caster2Box = document.getElementById("draft_caster2Box")
+    caster2Box.style.height = height
+    caster2Box.style.width = width
+    caster2Box.style.bottom = upper
+    caster2Box.style.left = "50.5vw"
+
+    let caster3Box = document.getElementById("draft_caster3Box")
+    caster3Box.style.height = height
+    caster3Box.style.width = width
+    caster3Box.style.bottom = bottom
+    caster3Box.style.left = "39vw"
+
+    document.getElementById("draft_caster1").textContent = caster1
+    document.getElementById("draft_caster2").textContent = caster2
+    document.getElementById("draft_caster3").textContent = caster3
+}
 
 document.getElementById("draft_blueName").textContent = blueName
 document.getElementById("blueScore").textContent = blueScore
 document.getElementById("draft_redName").textContent = redName
 document.getElementById("redScore").textContent = redScore
 document.getElementById("overlayImg").src = "../assets/" + casters + " Man Draft Overlay.png"
-// document.getElementById("draft_game").textContent = game 
-// document.getElementById("draft_patch").textContent = patch
