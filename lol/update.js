3ac49b4c9247c89d8ebb9ef339d892eb90bbd69f
bunnycou <noahjcou@gmail.com>
2024-11-21 02:29:10 -0500

    finished player names on draft screen

--- lol/update.js
+++ lol/update.js
@@ -3,6 +3,8 @@ function load() {
     let blueScore = localStorage.getItem("blueScore")
     let redName = localStorage.getItem("red")
     let redScore = localStorage.getItem("redScore")
+    let blueTeam = localStorage.getItem("blueTeam").split(",")
+    let redTeam = localStorage.getItem("redTeam").split(",")
     let game = localStorage.getItem("game")
     let patch = localStorage.getItem("patch")
     let potw = localStorage.getItem("potw")
@@ -17,18 +19,28 @@ function load() {
     if (!potw) { potw = "Everyone" }
     if (!motd) { motd = "" }
 
+    console.log("--Load Start--")
     try { document.getElementById("blueTeam").textContent = blueName } catch (error) {console.log("No blueTeam display")}
     try { document.getElementById("blueScore").textContent = blueScore } catch (error) { console.log("No blueScore display") }
     try { document.getElementById("redTeam").textContent = redName } catch (error) { console.log("No redTeam display") }
     try { document.getElementById("redScore").textContent = redScore } catch (error) { console.log("No redScore display") }
+    try { loadTeam(blueTeam, "blue") } catch (error) { console.log("No bluePlayer display") }
+    try { loadTeam(redTeam, "red") } catch (error) { console.log("No redPlayer display") }
     try { document.getElementById("game").textContent = game } catch (error) { console.log("No game num display") }
     try { document.getElementById("patch").textContent = patch } catch (error) { console.log("No patch display") }
     try { document.getElementById("potw").textContent = potw } catch (error) { console.log("No potw display") }
     try { document.getElementById("motd").textContent = motd } catch (error) { console.log("No motd display") }
-    console.log("--Load finished--")
 }
 
 setInterval(load, 1000)
+
+function loadTeam(team, side) {
+    document.getElementById(side+"Top").textContent = team[0]
+    document.getElementById(side+"Jgl").textContent = team[1]
+    document.getElementById(side+"Mid").textContent = team[2]
+    document.getElementById(side+"Bot").textContent = team[3]
+    document.getElementById(side+"Sup").textContent = team[4]
+}
 
 function getLatest() {
     return JSON.parse(Get("https://ddragon.leagueoflegends.com/api/versions.json"))[0].split(".").slice(0,2).join(".");
