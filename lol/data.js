aa27b5ec95ab5cf44812e0a589d01ced08e2d298
bunnycou <noahjcou@gmail.com>
2026-09-16 18:42:04 -0400

    Overhaul for new overlays, draft 1

--- lol/data.js
+++ lol/data.js
@@ -3,50 +3,53 @@ function loadvars() {
     let blueScore =  localStorage.getItem("blueScore")
     let redName = localStorage.getItem("red")
     let redScore = localStorage.getItem("redScore")
-    let blueTeam = localStorage.getItem("blueTeam").split(",")
-    let redTeam = localStorage.getItem("redTeam").split(",")
+    // let blueTeam = localStorage.getItem("blueTeam").split(",")
+    // let redTeam = localStorage.getItem("redTeam").split(",")
     let patch = localStorage.getItem("patch")
-    let potw = localStorage.getItem("potw")
+    let league = localStorage.getItem("league")
+    let static = localStorage.getItem("static")
     let motd = localStorage.getItem("motd")
 
     if (!blueName) { blueName = "Blue" }
     if (!blueScore) { blueScore = 0 }
     if (!redName) { redName = "Red" }
     if (!redScore) { redScore = 0 }
-    if (!blueTeam) { blueTeam = ["","","","",""] }
-    if (!redTeam) { redTeam = ["","","","",""] }
+    // if (!blueTeam) { blueTeam = ["","","","",""] }
+    // if (!redTeam) { redTeam = ["","","","",""] }
     if (!patch) { patch = getLatest() }
-    if (!potw) { potw = "Everyone" }
+    if (!league) { league = "Stout" }
+    if (!static) { static = "" }
     if (!motd) { motd = "" }
 
     document.getElementById("blue").value = blueName
     document.getElementById("blueScore").value = blueScore
     document.getElementById("red").value = redName
     document.getElementById("redScore").value = redScore
-    loadTeam(blueTeam, "blue")
-    loadTeam(redTeam, "red")
+    // loadTeam(blueTeam, "blue")
+    // loadTeam(redTeam, "red")
     document.getElementById("patch").value = patch
-    document.getElementById("potw").value = potw
+    document.getElementById("league").value = league
+    document.getElementById("static").value = static
     document.getElementById("motd").value = motd
 }
 
-function loadTeam(team, side) {
-    document.getElementById(side+"Top").value = team[0]
-    document.getElementById(side+"Jgl").value = team[1]
-    document.getElementById(side+"Mid").value = team[2]
-    document.getElementById(side+"Bot").value = team[3]
-    document.getElementById(side+"Sup").value = team[4]
-}
+// function loadTeam(team, side) {
+//     document.getElementById(side+"Top").value = team[0]
+//     document.getElementById(side+"Jgl").value = team[1]
+//     document.getElementById(side+"Mid").value = team[2]
+//     document.getElementById(side+"Bot").value = team[3]
+//     document.getElementById(side+"Sup").value = team[4]
+// }
 
-function getTeam(side) {
-    return [    
-        document.getElementById(side+"Top").value,
-        document.getElementById(side+"Jgl").value,
-        document.getElementById(side+"Mid").value,
-        document.getElementById(side+"Bot").value,
-        document.getElementById(side+"Sup").value
-    ]
-}
+// function getTeam(side) {
+//     return [    
+//         document.getElementById(side+"Top").value,
+//         document.getElementById(side+"Jgl").value,
+//         document.getElementById(side+"Mid").value,
+//         document.getElementById(side+"Bot").value,
+//         document.getElementById(side+"Sup").value
+//     ]
+// }
 
 function getLatest() {
     return JSON.parse(Get("https://ddragon.leagueoflegends.com/api/versions.json"))[0].split(".").slice(0,2).join(".");
@@ -64,21 +67,23 @@ function storevars() {
     let blueScore = document.getElementById("blueScore").value
     let red = document.getElementById("red").value
     let redScore= document.getElementById("redScore").value
-    let blueTeam = getTeam("blue")
-    let redTeam = getTeam("red")
+    // let blueTeam = getTeam("blue")
+    // let redTeam = getTeam("red")
     let patch = document.getElementById("patch").value
-    let potw = document.getElementById("potw").value
+    let league = document.getElementById("league").value
+    let static = document.getElementById("static").value
     let motd = document.getElementById("motd").value
 
     localStorage.setItem("blue", blue)
     localStorage.setItem("blueScore", blueScore)
     localStorage.setItem("red", red)
     localStorage.setItem("redScore", redScore)
-    localStorage.setItem("blueTeam", blueTeam)
-    localStorage.setItem("redTeam", redTeam)
+    // localStorage.setItem("blueTeam", blueTeam)
+    // localStorage.setItem("redTeam", redTeam)
     localStorage.setItem("game", parseInt(blueScore) + parseInt(redScore) + 1)
     localStorage.setItem("patch", patch)
-    localStorage.setItem("potw", potw)
+    localStorage.setItem("league", league)
+    localStorage.setItem("static", static)
     localStorage.setItem("motd", motd)
 }
 
@@ -87,22 +92,22 @@ function swapvars() {
     let blueScore = document.getElementById("blueScore").value
     let red = document.getElementById("red").value
     let redScore = document.getElementById("redScore").value
-    let blueTeam = getTeam("blue")
-    let redTeam = getTeam("red")
+    // let blueTeam = getTeam("blue")
+    // let redTeam = getTeam("red")
 
     localStorage.setItem("blue", red)
     localStorage.setItem("blueScore", redScore)
     localStorage.setItem("red", blue)
     localStorage.setItem("redScore", blueScore)
-    localStorage.setItem("blueTeam", redTeam)
-    localStorage.setItem("redTeam", blueTeam)
+    // localStorage.setItem("blueTeam", redTeam)
+    // localStorage.setItem("redTeam", blueTeam)
     
     document.getElementById("blue").value = red
     document.getElementById("blueScore").value = redScore
     document.getElementById("red").value = blue
     document.getElementById("redScore").value = blueScore
-    loadTeam("blue", redTeam)
-    loadTeam("red", blueTeam)
+    // loadTeam("blue", redTeam)
+    // loadTeam("red", blueTeam)
 }
 
 document.getElementById("submit").addEventListener("click", storevars);
--- lol/update.js
+++ lol/update.js
@@ -3,20 +3,23 @@ function load() {
     let blueScore = localStorage.getItem("blueScore")
     let redName = localStorage.getItem("red")
     let redScore = localStorage.getItem("redScore")
-    let blueTeam = localStorage.getItem("blueTeam").split(",")
-    let redTeam = localStorage.getItem("redTeam").split(",")
+    // let blueTeam = localStorage.getItem("blueTeam").split(",")
+    // let redTeam = localStorage.getItem("redTeam").split(",")
     let game = localStorage.getItem("game")
     let patch = localStorage.getItem("patch")
-    let potw = localStorage.getItem("potw")
+    let league = localStorage.getItem("league")
+    let static = localStorage.getItem("static")
     let motd = localStorage.getItem("motd")
 
     if (!blueName) { blueName = "Blue" }
     if (!blueScore) { blueScore = "0" }
     if (!redName) { redName = "Red" }
     if (!redScore) { redScore = "0" }
-    if (!game) { game = "Game 1" } else { game = "Game " + game }
+    // if (!game) { game = "Game 1" } else { game = "Game " + game } // game is originally an integer, red score + blue score
+    if (!game) { game = 1 }
     if (!patch) { patch = "Patch " + getLatest() } else { patch = "Patch " + patch }
-    if (!potw) { potw = "Everyone" }
+    if (!league) { league = "Stout" }
+    if (!static) { static = "" }
     if (!motd) { motd = "" }
 
     console.log("--Load Start--")
@@ -24,23 +27,26 @@ function load() {
     try { document.getElementById("blueScore").textContent = blueScore } catch (error) { console.log("No blueScore display") }
     try { document.getElementById("redTeam").textContent = redName } catch (error) { console.log("No redTeam display") }
     try { document.getElementById("redScore").textContent = redScore } catch (error) { console.log("No redScore display") }
-    try { loadTeam(blueTeam, "blue") } catch (error) { console.log("No bluePlayer display") }
-    try { loadTeam(redTeam, "red") } catch (error) { console.log("No redPlayer display") }
-    try { document.getElementById("game").textContent = game } catch (error) { console.log("No game num display") }
+    // try { loadTeam(blueTeam, "blue") } catch (error) { console.log("No bluePlayer display") }
+    // try { loadTeam(redTeam, "red") } catch (error) { console.log("No redPlayer display") }
+    // try { document.getElementById("game").textContent = game } catch (error) { console.log("No game num display") }
+    try { document.getElementById("draftOverlay").src = "Game"+game+"Draft.png" } catch (error) { console.log("No draftOverlay display") }
+    try { document.getElementById("mapOverlay").src = league+"Map.png" } catch (error) { console.log("No mapOverlay display") }
     try { document.getElementById("patch").textContent = patch } catch (error) { console.log("No patch display") }
-    try { document.getElementById("potw").textContent = potw } catch (error) { console.log("No potw display") }
+    try { document.getElementById("static").textContent = static } catch (error) { console.log("No static display") }
     try { document.getElementById("motd").textContent = motd } catch (error) { console.log("No motd display") }
+    console.log("--Load Done--")
 }
 
 setInterval(load, 1000)
 
-function loadTeam(team, side) {
-    document.getElementById(side+"Top").textContent = team[0]
-    document.getElementById(side+"Jgl").textContent = team[1]
-    document.getElementById(side+"Mid").textContent = team[2]
-    document.getElementById(side+"Bot").textContent = team[3]
-    document.getElementById(side+"Sup").textContent = team[4]
-}
+// function loadTeam(team, side) {
+//     document.getElementById(side+"Top").textContent = team[0]
+//     document.getElementById(side+"Jgl").textContent = team[1]
+//     document.getElementById(side+"Mid").textContent = team[2]
+//     document.getElementById(side+"Bot").textContent = team[3]
+//     document.getElementById(side+"Sup").textContent = team[4]
+// }
 
 function getLatest() {
     return JSON.parse(Get("https://ddragon.leagueoflegends.com/api/versions.json"))[0].split(".").slice(0,2).join(".");
